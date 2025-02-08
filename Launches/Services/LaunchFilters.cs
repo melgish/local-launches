b6e7ccb1699dae49b3f9018199cd7475e761177d
Nick <melgish@users.noreply.github.com>
2025-02-08 16:21:50 -0500

    Update to .net9 (#4)
    * remove deprecated attribute
    * Remove unused navigation attribute
    * Update to .net 9
    * use MapStaticAssets
    * update copyright
    * remove unnecessary framework
    * Fix missing namespace
    * spell check
    * update version

--- Launches/Services/LaunchFilters.cs
+++ Launches/Services/LaunchFilters.cs
@@ -2,16 +2,17 @@ using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 using Launches.Models;
 
+namespace Launches.Services;
+
 internal static partial class LaunchFilters {
     [ExcludeFromCodeCoverage]
     [GeneratedRegex("(canaveral|kennedy|patrick)", RegexOptions.IgnoreCase, "en-US")]
-    public static partial Regex SiteRegex();
+    private static partial Regex SiteRegex();
 
     /// <summary>
-    /// Test if launch site matches one of the common local names for the
-    /// Space Coast.
+    /// Filter to find launches on the Space Coast.
     /// </summary>
-    /// <param name="launch">launch to test</param>
+    /// <param name="launches">launch to test</param>
     /// <returns>true if a match, otherwise false</returns>
     public static IEnumerable<Launch> OnTheSpaceCoast(this IEnumerable<Launch> launches) =>
         launches.Where(launch => SiteRegex().IsMatch(launch.Site));
